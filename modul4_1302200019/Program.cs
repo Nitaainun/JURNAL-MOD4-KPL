041e49f460ea7f9ce8c309f20c2ed12b6b5bbf2f
Nita <nitaainun@student.telkomuniversity.ac.id>
2022-03-19 13:25:41 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul4_1302200019/Program.cs
+++ modul4_1302200019/Program.cs
@@ -10,6 +10,23 @@ namespace modul4_1302200019
     {
         static void Main(string[] args)
         {
+            Penjumlahan <long> penjumlahan = new Penjumlahan<long>(T);
+            long nim = 13 + 02 + 20;
+            nim.printAngka(nim);
+
         }
+    }
+    public class Penjumlahan<T>
+    {
+        private T jumlahTigaAngka;
+        public Penjumlahan(T jumlahTigaAngka)
+        {
+            this.jumlahTigaAngka = jumlahTigaAngka;
+        }
+        public void printAngka(T nim)
+        {
+            Console.WriteLine(nim);
+        }
+
     }
 }
